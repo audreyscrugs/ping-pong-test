2bf17817ddf05cf240e08f92eda252f5aa0b676b
Audrey Scrugham <audrey@epicodus.com>
2015-10-16 23:14:51 -0700

    Add ping-pong div, add correct brackets

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-$function() {
-  $(".btn").submit(function(event) {
+$(document).ready(function() {
+  $("#ping-pong").submit(function(event) {
     var number = prompt("Enter a number up to 100");
-    var int = NaN;
+    var int = 0;
     var name = "";
-  })
-}
+
+    for(int = 1; int <= number; int += 1) {
+      if(int % 3 === 0 && int % 5 === 0) {
+        name = "ping pong";
+      } else if(int % 3 === 0) {
+        name = "ping";
+      } else if(int % 5 === 0) {
+        name = "pong";
+      } else {
+        name = int;
+      }
+      $("#display").append("<li>" + name + "</li>");
+    }
+      event.preventDefault();
+  });
+});
